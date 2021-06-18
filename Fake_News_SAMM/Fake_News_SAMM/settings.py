114dfcc857f0791a8f2505f0e874adb1b617b9d4
Sean <hurleysn@bc.edu>
2021-06-18 00:09:37 -0400

    register/login finished up

--- Fake_News_SAMM/Fake_News_SAMM/settings.py
+++ Fake_News_SAMM/Fake_News_SAMM/settings.py
@@ -131,6 +131,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "/home" #Signals login function to redirect to homepage when login is successful 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 MEDIA_URL = '/media/'
+
+STATIC_URL = 'static/'
 
 LOGIN_URL = 'login'
