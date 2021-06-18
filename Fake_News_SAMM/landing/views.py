6ac0051a65a5c2d9be67b1b3d7342eddc963d290
Sean <hurleysn@bc.edu>
2021-06-17 21:29:49 -0400

    registration and login fit A-Team

--- Fake_News_SAMM/landing/views.py
+++ Fake_News_SAMM/landing/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
+from register.forms import RegisterForm
 
 
-
-def login(request):
-    return render(request, 'login.html')
+def index(request):
+    return render(request, 'index.html')
 
 
