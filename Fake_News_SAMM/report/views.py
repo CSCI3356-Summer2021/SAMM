6732a8b17f569a20f669548898e99f47b7e83065
Sean <hurleysn@bc.edu>
2021-06-22 18:36:14 -0400

    added remaining interfaces

--- Fake_News_SAMM/report/views.py
+++ Fake_News_SAMM/report/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def ReportMessage(request):
+    return render(request, 'report-message-modal.html')
+
+def ReportUser(request):
+    return render(request, 'report-user-modal.html')
