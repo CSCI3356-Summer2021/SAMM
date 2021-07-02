03fbbbe720cf5d584a0430c9e1baf50c6e4caad6
Alejandra I. Sanchez <42788950+alejandraisp@users.noreply.github.com>
2021-07-01 23:42:22 -0400

    messages models

--- Fake_News_SAMM/messaging/models.py
+++ Fake_News_SAMM/messaging/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class message(models.Model):
+    time_stamp = models.DateTimeField(verbose_name='publication_date', auto_now_add=True)
+    id = models.AutoField(primary_key=True)
+    sender_id = models.IntegerField(null=True)
+    receiver_id = models.IntegerField(null=True)
+    content = models.TextField(max_length=5000,blank=True,verbose_name="content")
+
