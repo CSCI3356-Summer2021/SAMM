9f05eead9c49099c7e04396d7d926db445d0a0b8
Alejandra I. Sanchez <42788950+alejandraisp@users.noreply.github.com>
2021-07-03 13:16:04 -0400

    messaging

--- Fake_News_SAMM/messaging/views.py
+++ Fake_News_SAMM/messaging/views.py
@@ -2,5 +2,30 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def messaging(request):
+    user = request.user
+    messaging = render.get_messages(user=user)
+    active_direct = None
+    directs = None
+
+    if messaging:
+        message = messaging[0]
+        active_direct = message['user'].username
+        directs = render.objects.filter(user=user, recipient= message['user'])
+        directs.update(is_read = True)
+
+        for message in messaging:
+            if message['user'].username == active_direct:
+                message['unread'] = 0
+        context = {
+            'directs' : directs,
+            'messaging' : message,
+            'active_direct' : active_direct,
+        }
+    template = loader.get_template('messaging/messaging.html')
+
+
+
     return render(request, 'messaging.html')
+
